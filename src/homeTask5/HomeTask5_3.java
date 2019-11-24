a9db7138f1409357a1baad26868a04fb1a04549b
Alexandra <aleksandrachka20@gmail.com>
2019-11-25 00:28:50 +0300

    homeTask 5 update

--- src/homeTask5/HomeTask5_3.java
+++ src/homeTask5/HomeTask5_3.java
@@ -1,24 +1,21 @@
 package homeTask5;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class HomeTask5_3 {
     public static void main(String[] args) {
         int[] array1 = {1, 5, 7, 2, 4, 3, 6};
-        String[] array2 = {"a", "b", "c", "d", "e"};
         ArrayList<Integer> array1List = new ArrayList<>();
-        ArrayList<String> array2List = new ArrayList<>();
-        for(int a1 : array1){
+        ArrayList<String> array2List = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e"));
+        for (int a1 : array1) {
             array1List.add(a1);
         }
-        for(String a2 : array2){
-            array2List.add(a2);
-        }
 
-        for(Integer a1 : array1List){
+        for (Integer a1 : array1List) {
             System.out.println(a1);
         }
-        for(String a2 : array2List){
+        for (String a2 : array2List) {
             System.out.println(a2);
         }
     }
